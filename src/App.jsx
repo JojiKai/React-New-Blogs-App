4d3de3e0dade7d134c6412d52fe84795da4dbcef
MENG KAI <whatever1668@gmail.com>
2025-09-25 14:28:51 +0800

    React JS Course-7.45_Time:01:00

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import News from "./Components/News";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="news-blog-app"></div>
+      <News />
+    </div>
+  );
+};
+
+export default App;
